5d7c30888e0bcea823c819e9ef5d12301d1bd4fb
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2024-09-28 00:34:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const createError = require('./utils/createError.js')
 // import cors from 'cors'
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000; 
 
 app.use(cors())
 app.use(express.json());
